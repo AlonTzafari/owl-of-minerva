c854f9710786c5491c78a7a40e7355a45d536f67
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-06-06 21:21:25 +0300

    add cron scheduler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import express from 'express';
+import api from './api';
+import cron from 'node-cron';
 const app = express();
 
+//TODO: serve static client
+
+//api route TODO: create routes
+app.use('/api', api);
+
+//scraper scheduler TODO: create & import scraper module
+cron.schedule('*/2 * * * *', () => {});
 
 
 export default app;
