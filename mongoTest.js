c6267f8a6035f96e265b653311fcf187cbd76818
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-06-07 13:17:30 +0300

    add db to docker-compose

---
+++ mongoTest.js
@@ -0,0 +1,41 @@
+// MONGO_INITDB_ROOT_USERNAME: root
+// MONGO_INITDB_ROOT_PASSWORD: secret
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const pasteSchema = new Schema({
+    title: String,
+    author: String,
+});
+
+const Paste = model('Paste', pasteSchema);
+
+
+const auth = {
+    user:'root',
+    password: 'secret'
+};
+
+mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true, useUnifiedTopology: true, auth, dbName:'dev'})
+.then(async res => {
+    console.log('connected successfully');
+    // await Paste.create({
+    //     title: 'My Title',
+    //     author: 'Alon'
+    // })
+    Paste.countDocuments((err, count) => {
+        if (err) throw err;
+        console.log('doc count: ' + count);
+    });
+    mongoose.connection.db.listCollections().toArray(function (err, names) {
+        console.log(names);
+    });
+
+})
+.catch(err => console.log(err))
+.finally(() => {
+    setTimeout(() => {mongoose.disconnect()}, 3000);
+})
+
+
+
--- proxyExample.js
+++ proxyExample.js
@@ -1,11 +1,12 @@
-const { SocksProxyAgent } = require('socks-proxy-agent');
 const axios = require('axios');
-
+const { SocksProxyAgent } = require('socks-proxy-agent');
 const agent = new SocksProxyAgent('socks5h://127.0.0.1:9050');
 
-axios.get('http://nzxj65x32vh2fkhk.onion/all?page=1', {httpAgent: agent})
-.then(({
-  data
-}) => {
-  console.log(data);
-});
+const getPage = () => {
+  axios.get('http://nzxj65x32vh2fkhk.onion/all?page=1', {httpAgent: agent})
+  .then(({
+    data
+  }) => {
+    console.log(data);
+  });
+}
